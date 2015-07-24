6f6566d2b1951e16582d10b993a91500edc9d387
Elijah <ejoleg@gmail.com>
2015-07-24 15:36:56 -0700

    more componentization

--- 004/script.jsx
+++ 004/script.jsx
@@ -1,51 +1,29 @@
-
-
-
 var OneUserGreeting = React.createClass({
   render: function(){
-    return <li>Hello {this.props.name}</li>
+    return(<li>Hello {this.props.name}</li>);
   }
 })
 
-var formComponent = React.createClass({
-  render: function(greet){
-    <form>
-      <input  placeholder="name" ref="name2greet"/>
-      <button onClick={greet}>Greet</button>
-    </form>
-  }
 
-})
+var FormComponent = React.createClass({
+  handleSubmit: function(event){
+    event.preventDefault();
+    this.props.greet(this.refs.name2greet);
+  },
+  render: function(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input placeholder="name" ref="name2greet"/>
+        <button type="button">Greet</button>
+      </form>
+    );
+  }
+});
 
 
 var Greetings = React.createClass({
-  getInitialState: function() {
-    return {
-      users: [
-        "Soren", "Todd", "Son", "James"
-      ]
-    };
-  },
-  greet: function() {
-    var ref = this.refs;
-    // var currentUsers = this.state.users;
-    // curretnUsers.push(ref.name2greet.value);
-    this.setState({
-      users: this.state.users.concat(this.refs.name2greet.value)
-    }, function() {
-      this.refs.name2greet.value = '';
-    });
-  },
-  // componentDidMount: function() {
-  // },
-// componentWillMount: function(){
-//   this.greet();
-// },
-// componentWillUnmount: function(){
-//   this.greet();
-// },
   render: function() {
-    var userLis = this.state.users.map(function(name, i) {
+    var userLis = this.props.users.map(function(name, i) {
       return <OneUserGreeting name={name} key={i} />
     })
     return (
@@ -58,15 +36,31 @@ var Greetings = React.createClass({
 
   }
 })
+
+
 var App = React.createClass({
+  getInitialState: function() {
+    return {users: ["Son", "Elijah"]};
+  },
+    // this.refs.name2greet
+  greet: function(nameInput) {
+    this.setState({
+      users: this.state.users.concat(nameInput.value)
+    }, function() {
+        nameInput.value = '';
+      }
+    );
+  },
   render: function() {
     return (
       <div>
-        <formComponent />
+        <FormComponent greet={this.greet} />
         <hr />
-        <Greetings />
+        <Greetings users={this.state.users} />
       </div>
     )
   }
 })
+
+
 React.render(<App/>, document.getElementById("root"));
