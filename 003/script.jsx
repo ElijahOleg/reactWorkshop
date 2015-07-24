2ade607bec2b8266352298dddf07d50ce41acae0
Elijah <ejoleg@gmail.com>
2015-07-24 14:32:50 -0700

    add a greeting component

--- 003/script.jsx
+++ 003/script.jsx
@@ -1,52 +1,61 @@
 
+
+
+var OneUserGreeting = React.createClass({
+  render: function(){
+    return <li>Hello {this.props.name}</li>
+  }
+})
+
+
 var Hello = React.createClass({
-  getInitialState: function(){
-    return { users: ["Soren","Todd","Son","James"]};
+  getInitialState: function() {
+    return {
+      users: [
+        "Soren", "Todd", "Son", "James"
+      ]
+    };
   },
-  greet: function(){
+  greet: function() {
     var ref = this.refs;
-    var curretnUsers = this.state.users;
-    curretnUsers.push(ref.name2greet.value);
+    // var currentUsers = this.state.users;
+    // curretnUsers.push(ref.name2greet.value);
     this.setState({
-      users:  this.state.users.concat(this.refs.name2greet.value)
-      },
-      function(){
-        this.refs.name2greet.value = '';
-      }
-    );
+      users: this.state.users.concat(this.refs.name2greet.value)
+    }, function() {
+      this.refs.name2greet.value = '';
+    });
   },
-  componentDidMount: function(){
-    this.greet();
-  },
-  // componentWillMount: function(){
-  //   this.greet();
+  // componentDidMount: function() {
   // },
-  // componentWillUnmount: function(){
-  //   this.greet();
-  // },
-  render: function(){
-    return(
+// componentWillMount: function(){
+//   this.greet();
+// },
+// componentWillUnmount: function(){
+//   this.greet();
+// },
+  render: function() {
+    var userLis = this.state.users.map(function(name, i) {
+      return <OneUserGreeting name={name} key={i} />
+    })
+    return (
       <div>
-        <input class="form" placeholder="name" ref="name2greet"/>
-        <button id="btn" onClick={this.greet}>Greet</button>
-        <br></br>
-        {this.state.greeting} {this.state.users}
+        <input  placeholder="name" ref="name2greet"/>
+        <button onClick={this.greet}>Greet</button>
+        <ul>
+          {userLis}
+        </ul>
       </div>
     )
   }
 })
-
 var App = React.createClass({
-  render: function(){
-    return(
+  render: function() {
+    return (
       <div>
-        <Hello />
+        <Hello/>
       </div>
     )
   }
 })
-
-React.render(
-  <App />,
-  document.getElementById("root")
-);
+React.render(<App/>, document.getElementById("root"));
